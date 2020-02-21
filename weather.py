45407beeca6738be297b1c4e25c19d1e4596705b
Arctic Fox <fox@x1team.com>
2020-02-21 19:10:57 +0200

    1) working on weather 2.0

--- weather.py
+++ weather.py
@@ -121,16 +121,42 @@ from keras.models import Sequential
 from keras import layers
 from keras.optimizers import RMSprop
 
+# model = Sequential()
+# model.add(layers.Flatten(input_shape=(lookback // step, float_data.shape[-1])))
+# model.add(layers.Dense(32, activation='relu'))
+# model.add(layers.Dense(1))
+#
+# model.compile(optimizer=RMSprop(), loss='mae')
+# history = model.fit_generator(train_gen, steps_per_epoch=500, epochs=20,
+#                               validation_data=val_gen, validation_steps=val_steps)
+
+
+
+from keras.models import Sequential
+from keras import layers
+from keras.optimizers import RMSprop
+
+# model = Sequential()
+# model.add(layers.GRU(32, input_shape=(None, float_data.shape[-1])))
+# model.add(layers.Dense(1))
+# model.compile(optimizer=RMSprop(), loss='mae')
+# history = model.fit_generator(train_gen,
+#                                       steps_per_epoch=500,
+#                                       epochs=20,
+#                                       validation_data=val_gen,
+#                                       validation_steps=val_steps)
+
+
 model = Sequential()
-model.add(layers.Flatten(input_shape=(lookback // step, float_data.shape[-1])))
-model.add(layers.Dense(32, activation='relu'))
+model.add(layers.GRU(32, dropout=0.1, recurrent_dropout=0.5, return_sequences=True, input_shape=(None, float_data.shape[-1])))
+model.add(layers.GRU(64, activation='relu', dropout=0.1, recurrent_dropout=0.5))
 model.add(layers.Dense(1))
-
 model.compile(optimizer=RMSprop(), loss='mae')
-history = model.fit_generator(train_gen, steps_per_epoch=500, epochs=20,
-                              validation_data=val_gen, validation_steps=val_steps)
-
-
+history = model.fit_generator(train_gen,
+                              steps_per_epoch=500,
+                              epochs=40,
+                              validation_data=val_gen,
+                              validation_steps=val_steps)
 
 import matplotlib.pyplot as plt
 loss = history.history['loss']
@@ -143,3 +169,5 @@ plt.plot(epochs, val_loss, 'b', label='Validation loss')
 plt.title('Training and validation loss')
 plt.legend()
 plt.show()
+
+
